de038e47ccd977ef8a417c7f9002c99bb2bc6e03
nqdinh-1203 <quangdinhnt1203@gmail.com>
2023-03-30 20:36:03 +0700

    node-ts-boilerplate

---
+++ src/routes/site.route.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import siteController from '../controllers/SiteController';
+
+const router = Router();
+
+router.get('/', siteController.index);
+
+export default router;
